a3425c058d30bfb8d58f6b37c3831cfafcf5c66e
mattferreira <mattferreira7@gmail.com>
2014-07-07 20:17:51 -0400

    init

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,5 @@
+client = Yelp::Client.new({ consumer_key: '9e0zibHyEJGX9EB5b4Ej-g',
+                            consumer_secret: 'tsL_Zn9UkELpd8dvRrtvalcKyxc',
+                            token: 'wxOWRm2jMM030PhTdu0Zn9V96Nb5pxyD',
+                            token_secret: '9rwTIKmCQZoeMelyLFbsjCYSeT4'
+                          })
